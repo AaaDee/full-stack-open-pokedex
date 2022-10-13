92f04fce053a74f9741ccc996f9b91f440358940
Aarne Rissanen <ari.outermedia.de>
2022-10-13 22:33:07 +0200

    11.12

--- health_check.sh
+++ health_check.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-echo "Hello from shell script"
-
-exit 1 # exit status 1 means that the script "fails"
+response=$(curl /health)
+if [ $response == "ok" ]
+then 
+  echo "ok received"
+  exit 0
+else 
+  echo "ok not receiver"
+  exit 1
+fi
